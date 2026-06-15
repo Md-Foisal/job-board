28651f3196cfea059a6c7bd4350794a04030dafd
MD. FOISAL <mffoisal8@gmail.com>
2026-06-15 10:00:48 +0600

    feat: add Notification for job application submission

--- app/Listeners/SendApplicationConfirmation.php
+++ app/Listeners/SendApplicationConfirmation.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\JobApplicationSubmitted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Notifications\ApplicationSubmittedNotification;
 
 class SendApplicationConfirmation
 {
@@ -21,6 +22,6 @@ class SendApplicationConfirmation
      */
     public function handle(JobApplicationSubmitted $event): void
     {
-        logger('Application submitted: ' . $event->application->id);
+        $event->application->user->notify(new ApplicationSubmittedNotification($event->application));
     }
 }
